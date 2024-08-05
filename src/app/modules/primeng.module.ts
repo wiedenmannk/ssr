b7cf0243582f429d8dd652019ac6ecc71d5e0228
wiedenmannk <klaus.wiedenmann@yahoo.de>
2024-08-05 22:11:57 +0200

    boxen fertiggestellt

--- src/app/modules/primeng.module.ts
+++ src/app/modules/primeng.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from "@angular/core";
 
 import { ButtonModule } from "primeng/button";
 import { MenubarModule } from "primeng/menubar";
+import { CardModule } from "primeng/card";
 
-const modules = [ButtonModule, MenubarModule];
+const modules = [ButtonModule, MenubarModule, CardModule];
 
 @NgModule({
 	declarations: [],
